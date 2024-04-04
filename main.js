a7e966bdcff646fe3b31b67a4029c88645f12db8
xusniya123 <xusniya12345678@gmail.com>
2024-04-04 20:34:43 +0500

    Lesson 5 moth (1)

---
+++ main.js
@@ -0,0 +1,76 @@
+let day = +prompt("plaese enter any number ..");
+let dayName;
+
+switch (day) {
+
+    case 1:
+        dayName = "Monday";
+        break;
+    case 2:
+        dayName = "Tuesday";
+        break;
+    case 3:
+        dayName = "Wednesday";
+        break;
+    case 4:
+        dayName = "Thursday";
+        break;
+    case 5:
+        dayName = "Friday";
+        break;
+    case 6:
+        dayName = "Saturday";
+        break;
+    case 7:
+        dayName = "Sunday";
+        break;
+    default:
+        dayName = "Invalid day";
+
+}
+
+alert("Today is = " + dayName);
+
+
+// let day = +prompt("Biror bir son kiriting");
+// let dayName;
+
+// switch (day) {
+//     case 0:
+//         dayName = ["Madinaxon", "Xusniya", "Nasiba", "Muhammadjon" ,"Munavvar"];
+//       for (const name of dayName) {
+//    console.log(name);
+//       }
+//         break;
+//     case 1:
+//         dayName = "Mars IT School";
+//             for (let i =0; i < 10; i++){
+//  console.log("Mars IT School");
+//  alert("Mars IT School");
+//  }
+//         break;
+//     case 2:
+//         dayName = (Math.pow(2,3) + Math.pow (3,2))/3;
+//         break;
+//     case 3:
+//         dayName = ""
+//         break;
+//     case 4:
+//         dayName = "Thursday";
+//         break;
+//         case 5:
+//             dayName = "Friday";
+//             break;
+//             case 6:
+//                 dayName =Math.round (5.6);
+//                 break;
+//     default:
+//         dayName = "Invalid day";
+
+// }
+
+// alert( "Today is =" + dayName);
+
+
+
+
